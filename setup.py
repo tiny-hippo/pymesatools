01296d223aad4d321e31eb101d71b46d5abcedde
tiny-hippo <simonmueller.ch@gmail.com>
2022-12-20 15:41:20 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name="mesatools",
     version="0.1",
     description="various tools to handle MESA with python",
-    url="https://github.com/tiny-hippo/pyMesaToolbox",
+    url="https://github.com/tiny-hippo/pymesatools",
     author="Simon Müller",
-    author_email="simon.mueller7@uzh.ch",
+    author_email="simonandres.mueller@uzh.ch",
     license="GNU GPLv3",
     packages=find_packages(include=["mesatools", "mesatools.*"]),
     install_requires=["numpy", "matplotlib", "f90nml", "mesa_reader"]
